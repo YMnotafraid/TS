6eff2a496d7a4539311dbe207b00f35052b31e60
YMnotafraid <867568537@qq.com>
2023-04-04 19:18:41 +0800

    02

---
+++ 02.basic.1.ts
@@ -0,0 +1,9 @@
+let sym: symbol = Symbol("a");
+let sym2: Symbol = Symbol("b");
+// sym = sym2;
+sym2 = sym;
+
+let str: String = new String("a");
+let str2: string = "a";
+str = str2;
+// str2 = str;
